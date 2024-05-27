f01b9d44c3bcaa985fd8113f94d3455cb1e1bc46
mikemuturi <mikeirungumuturi@gmail.com>
2024-05-27 18:10:51 +0300

    Mongo db intergration

--- app.js
+++ app.js
@@ -1,62 +1,108 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const { result } = require('lodash');
 
-//express app
+// express app
 const app = express();
 
+// connect to db
+// const dbURI = 'mongodb+srv://Mike_1234:Mike_1234@test-123.0pj0urc.mongodb.net/Test-123';
+// mongoose.connect(dbURI)
+//   .then((result) => {
+//     console.log('Connected to DB');
+//     // listen for requests only after DB connection is established
+//     const port = process.env.PORT || 3000; // Use the environment variable PORT or default to 3000
+//     app.listen(port, () => {
+//       console.log(`Server is running on port ${port}`);
+//     });
+//   })
+//   .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-
-
-// listen for requests
-
-app.listen(3000);
-
-// midlle ware and static files
+// middleware and static files
 app.use(express.static('public'));
+app.use(morgan('dev'));
 
+// app.get('/add-blog', (req, res) => {
+// const blog = new Blog(
+//     {
+//         title: 'New blog',
+//         snippet: 'about my new blog',
+//         body: 'more about my new blog'
+//     }
+// );
 
-//  app.use((req, res, next) => {
-//     console.log('new request made:');
-//     console.log('host:, req.hostname');
-//     console.log('path:', req.path);
-//     console.log('method', req.method);
-//     next();
-//  });
+// blog.save()
+// .then( (result) => {
+//     res.send(result)
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+// });
 
- app.use(morgan('dev'));
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//     .then((result) => {
+//         res.send(result);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// });
+// app.get('/single-blog', (req, res) =>{
+//     Blog.findById()
+//     .then((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// })
+
+// routes
+// app.get('/', (req, res) => {
+//   const blogs = [
+//     { title: 'Mike finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+//     { title: 'Muturi finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+//     { title: 'How to defeat Bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+//   ];
+//   res.render('index', { title: 'Home', blogs });
+// });
 
 app.get('/', (req, res) => {
-    // res.send('<p>Home Page</p>');
-    // res.sendFile('./views/index.html', {root:__dirname});
-    const blogs = [
-        {title: 'Mike finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Muturi finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', {title: 'Home',  blogs});
-});
+    res.redirect('/blogs');
+})
 
 app.get('/about', (req, res) => {
-    // res.send('<p>About Page</p>');
-    // res.sendFile('./views/about.html', {root:__dirname});
-    res.render('about', {title: 'About'})
+  res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create'});
+//blogs route
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1})
+    .then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result })
+    }).catch((err) =>{
+      console.log(err);  
+    })
 })
+
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create' });
+});
 
 // redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
 
-app.get('/about-us', (req, res) =>{
-    res.redirect('/about',  {title: 'About'});
-})
-
-//404 Page
-app.use((req, res) =>{
-    // res.status(404).sendFile('./views/404.html', {root: __dirname});
-    res.status(404).render('404', {title: '404'});
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
